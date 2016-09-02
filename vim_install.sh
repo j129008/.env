df58775d7b69fa00a943e4842b97a54c3141ac4d
j129008 <j129008@gmail.com>
2016-09-02 15:29:44 +0800

    copy gitconfig file

---
+++ vim_install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ./.vimrc ~/.vimrc
+vim +PluginInstall +qall
---
+++ zsh_install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+cp $ZSH_CUSTOM/plugins/zsh-autosuggestions/src/config.zsh $ZSH_CUSTOM
+vim +9 $ZSH_CUSTOM/config.zsh
+
+# install autojump
+git clone git://github.com/joelthelion/autojump.git /tmp
+./tmp/install.py
+
+# copy gitconfig
+cp ./.gitconfig ~
