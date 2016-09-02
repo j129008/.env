befaeed8f58afffb817282fcb50d835aa903dc41
j129008 <j129008@gmail.com>
2016-09-02 15:41:47 +0800

    repair bash path

--- vim_install.sh
+++ vim_install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ./.vimrc ~/.vimrc
 vim +PluginInstall +qall
--- zsh_install.sh
+++ zsh_install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
